4fade44d71c0d60cdb6f4092add8d265f7b15d78
Mangesh Jaybhaye <mangeshjaybhaye3@gmail.com>
2025-09-28 20:23:18 +0530

    Implemented Home section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>hello</div>
+   <Hero/>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,23 @@
 import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
-import { HomeIcon, LogIn, Sprout } from "lucide-react";
+import {
+  HomeIcon,
+  LogIn,
+  LogInIcon,
+  LogOut,
+  LogOutIcon,
+  Sprout,
+} from "lucide-react";
 import { ModeToggle } from "./ModeToggle";
 import { stackServerApp } from "@/stack/server";
+import { getUserDetails } from "@/actions/user.action";
+import { UserButton } from "@stackframe/stack";
 
 async function Navbar() {
-  const user = stackServerApp.getUser();
-  const app=stackServerApp.urls;
+  const user = await stackServerApp.getUser();
+  const app = stackServerApp.urls;
+  const userProfile = await getUserDetails(user?.id);
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
@@ -20,6 +30,12 @@ async function Navbar() {
               🌱 Plantventory
             </Link>
           </div>
+
+          {userProfile?.name && (
+            <span className="text-[14px] text-gray-600 dark:text-gray-300">{`Hello, ${
+              userProfile?.name.split("")[0]
+            }`}</span>
+          )}
           <div className="hidden md:flex items-center space-x-4">
             <Button variant="ghost" className="flex items-center gap-2" asChild>
               <Link href="/plants">
@@ -36,12 +52,34 @@ async function Navbar() {
             </Button>
             <ModeToggle />
 
-            <Button variant="ghost" className="flex items-center gap-2" asChild>
-              <Link href={app.signIn}>
-                <LogIn className="w-4 h-4" />
-                <span className="hidden lg:inline">Sign In</span>
-              </Link>
-            </Button>
+            {user ? (
+              <>
+                <Button
+                  variant="secondary"
+                  className="flex items-center gap-2"
+                  asChild
+                >
+                  <Link href={app.signOut}>
+                    <LogOut className="w-4 h-4" />
+                    <span className="hidden lg:inline">Sign Out</span>
+                  </Link>
+                </Button>
+                <UserButton/>
+              </>
+            ) : (
+              <>
+                <Button
+                  variant="ghost"
+                  className="flex items-center gap-2"
+                  asChild
+                >
+                  <Link href={app.signIn}>
+                    <LogIn className="w-4 h-4" />
+                    <span className="hidden lg:inline">Sign In</span>
+                  </Link>
+                </Button>
+              </>
+            )}
           </div>
         </div>
       </div>
